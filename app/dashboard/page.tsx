c2999d8bcf1a45a0707072145f8237e03a68fd41
Shirley <shirley.xu.com@gmail.com>
2025-09-09 12:21:48 -0400

    Edit frontend

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,8 +4,8 @@ import React, { useState, useEffect } from "react";
 import Home from "../components/Home";
 
 export default async function DashboardPage() {
-  const session = await getServerSession(authOptions);
-  if (!session) return <div>Please log in</div>;
+  // const session = await getServerSession(authOptions);
+  // if (!session) return <div>Please log in</div>;
 
   return (
     <div>
